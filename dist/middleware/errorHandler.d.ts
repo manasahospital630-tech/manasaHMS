e56fd06c5208427dc47240aa29711fa863c3e656
Manasa Hospital <info@manasahospital.org>
2026-08-09 09:45:11 +0530

    build: update frontend dist assets

--- dist/middleware/errorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class AppError extends Error {
-    statusCode: number;
-    isOperational: boolean;
-    constructor(message: string, statusCode?: number, isOperational?: boolean);
-}
-export declare const errorHandler: (err: Error | AppError, _req: Request, res: Response, _next: NextFunction) => void;
-//# sourceMappingURL=errorHandler.d.ts.map
