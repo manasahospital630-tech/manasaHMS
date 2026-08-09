3003a8f010d1212477c28e2215be5956c99ad8ab
Manasa Hospital <info@manasahospital.org>
2026-08-09 22:55:10 +0530

    build: update frontend dist assets

--- dist/utils/s3Upload.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const uploadBase64Image: (base64String: string, folderCategory?: string, customFileName?: string) => Promise<string>;
-export declare const generateAndUploadQrCode: (textToEncode: string, itemId: string) => Promise<string>;
-//# sourceMappingURL=s3Upload.d.ts.map
